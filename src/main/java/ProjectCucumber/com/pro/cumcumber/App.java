2c7f40a768367148210f600315e9e63ca6b6991e
Ruchika Karki <ruchika.karki@gmail.com>
2023-09-12 13:51:07 +0200

    first commit'

---
+++ src/main/java/ProjectCucumber/com/pro/cumcumber/App.java
@@ -0,0 +1,7 @@
+package ProjectCucumber.com.pro.cumcumber;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/CucumberPackage/SecondClass.java
@@ -0,0 +1,11 @@
+package CucumberPackage;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is second class");
+
+	}
+
+}
